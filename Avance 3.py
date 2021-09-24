82d4be1dbb71a784587de512ae807c754ed52198
RafaelDiazH <88898032+RafaelDiazH@users.noreply.github.com>
2021-09-24 11:43:06 -0500

    Add files via upload

--- Avance 3.py
+++ Avance 3.py
@@ -2,7 +2,7 @@ def evalua_mult(val_1, val_2):
     return val_1*val_2
 def evalua_div(val_1, val_2):
     return val_1/val_2
-lim = 100
+x = 100
 obtenidos = int(input())
 print("el total de puntos fue", obtenidos)
 minas_detonadas = int(input())
@@ -12,9 +12,8 @@ print("tu mejor racha sin minas fue de", racha_mayor)
 div = evalua_div(obtenidos, minas_detonadas)
 multi = evalua_mult(div, racha_mayor)
 print("El total de puntos es ", multi)
-while multi > lim:
+while x <= multi:
     print("Felicidades, pasaste el nivel")
-while multi < lim:
+while multi <= x:
     print("Lastima, mejor suerte a la próxima")
-
     
